793483acc93ab1980840a45936467d22217b1519
kvutien <kvutien.yes@gmail.com>
2021-11-14 19:36:22 +0100

    add bottom navigation bar

--- 03-basic-widgets/projects/starter/lib/main.dart
+++ 03-basic-widgets/projects/starter/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'fooderlich_theme.dart';
+import 'home.dart';
 
 void main() {
   runApp(const Fooderlich());
@@ -9,24 +10,12 @@ class Fooderlich extends StatelessWidget {
   const Fooderlich({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    // TODO: Create theme
     final theme = FooderlichTheme.dark() ;
     // TODO: Apply Home widget
     return MaterialApp(
-      // TODO: Add theme
+      theme: theme,
       title: 'Fooderlich',
-      // 4
-      home: Scaffold(
-        // TODO: Style the title
-        appBar: AppBar(
-          title: Text('Fooderlich', style: theme.textTheme.headline6),
-        ),
-        // TODO: Style the body text
-        body: Center(
-            child: Text('Let\'s get cooking 👩‍🍳',
-              style: theme.textTheme.headline1,)
-        ),
-      ),
+      home: const Home(),
     );
   }
 }
